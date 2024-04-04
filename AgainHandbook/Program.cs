ac3ef20092627729f0d0ecc52c6272b5237ef7c3
har1nagle <hnagle.0@yahoo.com>
2024-04-04 10:10:08 +0530

    ICategoryRepository use or apply

--- AgainHandbook/Program.cs
+++ AgainHandbook/Program.cs
@@ -1,3 +1,5 @@
+using Again.DataAccess.Repository;
+using Again.DataAccess.Repository.IRepository;
 using Again.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 
 var app = builder.Build();
