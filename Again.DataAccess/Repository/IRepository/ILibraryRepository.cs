9c77f2e5247b8a1b2d7f572e2cca236cf54b48aa
har1nagle <hnagle.0@yahoo.com>
2024-04-04 11:45:19 +0530

    Library Repository created

---
+++ Again.DataAccess/Repository/IRepository/ILibraryRepository.cs
@@ -0,0 +1,15 @@
+﻿using AgainHandbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Again.DataAccess.Repository.IRepository
+{
+    public interface ILibraryRepository : IRepository<Library>
+    {
+        void Update(Library obj);
+
+    }
+}
---
+++ Again.DataAccess/Repository/LibraryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Again.DataAccess.Repository.IRepository;
+using Again.DataAcess.Data;
+using AgainHandbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Again.DataAccess.Repository
+{
+    public class LibraryRepository : Repository<Library>, ILibraryRepository
+    {
+        private ApplicationDbContext _db;
+        public LibraryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Library obj)
+        {
+            _db.Libraries.Update(obj);
+        }
+    }
+}
