fb124b15c50200e938a65f490b35e27e3db2439b
har1nagle <hnagle.0@yahoo.com>
2024-04-04 09:55:26 +0530

    IRepository done

--- Again.DataAccess/Data/ApplicationDbContext.cs
+++ Again.DataAccess/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace Again.DataAcess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Library> Libraries { get; set; }
+        //public DbSet<Library> Libraries { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,12 +21,12 @@ namespace Again.DataAcess.Data
                 new Category { Id = 2, Name = "Circular", DisplayOrder = 2 }
                 );
 
-            modelBuilder.Entity<Library>().HasData(
-                new Library { Id = 1, Name = "Sos file test", CategoryId = 1},
-                new Library { Id = 2, Name = "circular file test", CategoryId = 2 },
-                new Library { Id = 3, Name = " I am new file test", CategoryId = 1 }
+            //modelBuilder.Entity<Library>().HasData(
+            //    new Library { Id = 1, Name = "Sos file test", CategoryId = 1},
+            //    new Library { Id = 2, Name = "circular file test", CategoryId = 2 },
+            //    new Library { Id = 3, Name = " I am new file test", CategoryId = 1 }
 
-                );
+            //    );
         }
 
     }
